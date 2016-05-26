6b451024f219baa6a2fa7da63329345d3eb8222b
shawenlx <liuxi@andware.cn>
2016-05-26 20:23:23 +0800

    Initial Commit

---
+++ FDTemplateCellTableTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDTemplateCellTableTest
+//
+//  Created by Apple on 16/5/26.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
